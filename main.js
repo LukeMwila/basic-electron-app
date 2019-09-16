6d8d5f119530ba3fb02f2ce4ba068054397acdd6
Lukonde Francis Mwila <lukonde.mwila@entelect.co.za>
2019-09-16 08:02:38 +0200

    Added an ipc worker and updated the script to automatically install and restart the app after download

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const path = require("path");
 const url = require("url");
@@ -9,6 +9,7 @@ const url = require("url");
  * automatically when the JS object is garabge collected.
  */
 let win;
+let workerWindow;
 
 function createWindow() {
   // Create browser window
@@ -29,18 +30,42 @@ function createWindow() {
     })
   );
 
+  // create hidden worker window
+  workerWindow = new BrowserWindow({
+    show: false,
+    webPreferences: { nodeIntegration: true }
+  });
+
+  // this will load the worker.html file
+  workerWindow.loadFile("worker.html");
+
   // Open devtools
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
   win.on("closed", () => {
     win = null;
   });
 }
 
+function sendWindowMessage(targetWindow, message, payload) {
+  if (typeof targetWindow === "undefined") {
+    console.log("Target window does not exist");
+    return;
+  }
+  console.log("[Step 2]: The message has been sent by the worker");
+  console.log("[Message Payload]: ", payload);
+  targetWindow.webContents.send(message, payload);
+}
+
 // Run create window function
-app.on("ready", () => {
+app.on("ready", async () => {
   createWindow();
   autoUpdater.checkForUpdatesAndNotify();
+
+  ipcMain.on("message-from-worker", (event, arg) => {
+    console.log("[Step 1]: Here sending message from worker");
+    sendWindowMessage(win, "message-from-worker", arg);
+  });
 });
 
 // Quit when all windows are closed
